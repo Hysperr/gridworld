2e6c446f618a5a6f12a80f77c6269561b8e66c29
Hysperr <Hysperr@users.noreply.github.com>
2017-05-02 17:50:04 -0400

    Initial Upload

---
+++ src/Location.java
@@ -0,0 +1,20 @@
+
+public class Location {
+    private int x;
+    private int y;
+
+    protected void setX(int x) {
+        this.x = x;
+    }
+    protected void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,8 @@
+
+public class Square {
+    private enum DirType { UP, DOWN, LEFT, RIGHT }
+    private float [] weights = new float[4];
+    private float [] eleg = new float[4];
+    private int rewardval;
+
+}
