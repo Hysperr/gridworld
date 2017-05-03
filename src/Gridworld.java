ad642aa2fa5375b0ccf512ef4b0609b2db2c03b5
Hysperr <Hysperr@users.noreply.github.com>
2017-05-03 01:28:28 -0400

    update, algorithm implementation nearly finish

--- src/Gridworld.java
+++ src/Gridworld.java
@@ -7,6 +7,7 @@ public class Gridworld {
     private Location mygoal;
     private float gammaDF = 0.9f;
     private float lambda = 0.000005f;
+    private float alpha = 0.005f;
 
 
 
@@ -28,6 +29,7 @@ public class Gridworld {
         }
         mylocation = new Location(); startMyLocation();
         mygoal = new Location(); startMyGoal();
+        board[mygoal.getX()][mygoal.getY()].setRewardval(1);
     }
 
     public void printBoard() {
@@ -39,7 +41,7 @@ public class Gridworld {
                     System.out.print("X ");
                 }
                 else {
-                    System.out.print("- ");
+                    System.out.print(board[i][j].getDirType());
                 }
             }
             System.out.println();
@@ -77,9 +79,9 @@ public class Gridworld {
         mygoal.setY(col);
     }
 
-    public boolean isfound() {
+    public boolean onTarget() {
         if (mylocation.getX() == mygoal.getX() && mylocation.getY() == mygoal.getY()) {
-            System.out.println("Found at location (" + mylocation.getX() + "," + mylocation.getY() + ")");
+//            System.out.println("Found at location (" + mylocation.getX() + "," + mylocation.getY() + ")");
             return true;
         }
         return false;
@@ -88,7 +90,7 @@ public class Gridworld {
     public Square.DirType explore() {
         Random r = new Random();
         int move = r.nextInt(Square.DirType.values().length);
-        return Square.DirType.values()[move];
+        return Square.DirType.values()[move];   // next move
     }
 
     public Square.DirType exploit() {
@@ -125,13 +127,154 @@ public class Gridworld {
                 nextMove = Square.DirType.RIGHT;
                 break;
             default:
-               throw new IllegalArgumentException("You shouldn't be seeing this, ever");
+                throw new IllegalArgumentException("You shouldn't be seeing this, ever. Dir was " +
+                        "" + nextMove + " from mylocation (" + mylocation.getX() + "," +
+                        "" + mylocation.getY() + ")");
         }
 
         return nextMove;
-
     }
 
+    public void printMyLocation() {
+        System.out.println("My location (" + mylocation.getX() + "," + mylocation.getY() + ")");
+    }
+    public void printGoalLocation() {
+        System.out.println("Goal location (" + mygoal.getX() + "," + mygoal.getY() + ")");
+    }
+
+    public Square get_qsaP(Square.DirType dirType) {
+        switch (dirType) {
+            case UP:
+                return board[mylocation.getX() - 1][mylocation.getY()];
+            case DOWN:
+                return board[mylocation.getX() + 1][mylocation.getY()];
+            case LEFT:
+                return board[mylocation.getX()][mylocation.getY() - 1];
+            case RIGHT:
+                return board[mylocation.getX()][mylocation.getY() + 1];
+            default:
+                throw new IllegalArgumentException("You shouldn't be seeing this, ever. Actual Param dir was " +
+                        "" + dirType + " from mylocation (" + mylocation.getX() + "," +
+                        "" + mylocation.getY() + ")");
+        }
+    }
+
+    private float maxOfSquare(Square s) {
+
+        float max = -100f;
+        int idx;
+
+        float upVal = s.getWeights()[0];
+        float downVal = s.getWeights()[1];
+        float leftVal = s.getWeights()[2];
+        float rightVal = s.getWeights()[3];
+
+        float[] farray = {upVal, downVal, leftVal, rightVal};
+        for (int i = 0; i < farray.length; i++) {
+            if (farray[i] > max) {
+                max = farray[i];
+                idx = i;
+            }
+        }
+
+        return max;
+        // we don't know where it leaves out but that's fine. algorithm takes care of messy details.
+        // However can make it return the next move...see optional function below
+    }
+    private Square.DirType nextMoveBasedonMaxofSquare(Square s) {
+        // this is basically exploit, except can be any square here.
+        // exploit is always our current location
+
+        float max = -100f;
+        int idx = -1;
+
+        float upVal = s.getWeights()[0];
+        float downVal = s.getWeights()[1];
+        float leftVal = s.getWeights()[2];
+        float rightVal = s.getWeights()[3];
+
+        float[] farray = {upVal, downVal, leftVal, rightVal};
+        for (int i = 0; i < farray.length; i++) {
+            if (farray[i] > max) {
+                max = farray[i];
+                idx = i;
+            }
+        }
+
+        return Square.DirType.values()[idx];
+    }
+
+
+    public void takeAction() {
+        Random r = new Random();
+        float tmp = (r.nextInt(100) + 1) / 100f;
+        Square.DirType nextMove_qsa = (tmp < gammaDF) ? exploit() : explore();
+
+        // Q(s,a) and next move for Q(s,a) is above
+        Square qsa = board[mylocation.getX()][mylocation.getY()];
+        float max_qsa = maxOfSquare(qsa);
+
+        int reward;
+        float max_qsaP;
+        Square qsaP;
+
+        try {
+            switch (nextMove_qsa) {
+                case UP:
+                    qsaP = get_qsaP(Square.DirType.UP);
+                    max_qsaP = maxOfSquare(qsaP);
+                    reward = qsaP.getRewardval();
+                    /* Another way to do it.
+                    Square.DirType nextMove_qsaP = nextMoveBasedonMaxofSquare(get_qsaP(Square.DirType.UP));
+                    max_qsaP = qsaP.getWeights()[dirType.ordinal()];
+                    */
+                    break;
+                case DOWN:
+                    qsaP = get_qsaP(Square.DirType.DOWN);
+                    max_qsaP = maxOfSquare(qsaP);
+                    reward = qsaP.getRewardval();
+                    break;
+                case LEFT:
+                    qsaP = get_qsaP(Square.DirType.LEFT);
+                    max_qsaP = maxOfSquare(qsaP);
+                    reward = qsaP.getRewardval();
+                    break;
+                case RIGHT:
+                    qsaP = get_qsaP(Square.DirType.RIGHT);
+                    max_qsaP = maxOfSquare(qsaP);
+                    reward = qsaP.getRewardval();
+                    break;
+                default:
+                    throw new IllegalArgumentException("You shouldn't be seeing this, ever");
+            }
+        }
+        catch (NullPointerException|IllegalArgumentException e) {
+            max_qsaP = 0;
+            reward = -1;
+        }
+        // we now have Q(s'a')
+
+        // delta = r  + gamma * Q(s'a') - Q(s,a)
+        float delta = reward + gammaDF * max_qsaP - max_qsa;
+        // e(s,a) <- e(s,a) + 1
+        qsa.getEleg()[nextMove_qsa.ordinal()] += 1;
+        // for all s,a:
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                for (int k = 0; k < 4; k++) {
+                    // Q(s,a) <- Q(s,a) + alpha * delta * e(s,a)
+                    board[i][j].getWeights()[k] += (alpha * delta * board[i][j].getEleg()[k]);
+                    // e(s,a) <- gamma * lambda * e(s,a)
+                    board[i][j].getEleg()[k] = gammaDF * lambda * board[i][j].getEleg()[k];
+                }
+            }
+        }
+
+
+
+
+
+    }
 
 
 
@@ -139,6 +282,9 @@ public class Gridworld {
         Gridworld g = new Gridworld(3, 3);
         g.printBoard();
         g.printSquare(2, 2);
+        Square.DirType dirType = Square.DirType.LEFT;
+        System.out.println(dirType.ordinal());
+
     }
 
 }
